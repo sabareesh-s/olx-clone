05739a1f774a3099111e0fa098b71aebdde3cf20
Sabareesh S <sabareesh1221@gmail.com>
2021-10-30 00:17:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div className='HomeClass'>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
